8e4e2766f9abe4bd87e60dfb7ea9036febbcc431
Sadeu Michel <michel.sadeu@yahoo.fr>
2021-01-02 16:19:47 +0100

    Création de formulaire pour la création des pints

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
+use App\Form\PinType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class PinsController extends AbstractController
 {
     /**
      * @Route("/", name="app_pins")
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
+        $pin = new Pin;
+        $form = $this->createForm(PinType::class, $pin);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($pin);
+            $em->flush();
+        }
+
         return $this->render('pins/index.html.twig', [
-            'controller_name' => 'PinsController',
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -23,7 +23,7 @@ class Pin
     private $title;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="text")
      */
     private $content;
 
