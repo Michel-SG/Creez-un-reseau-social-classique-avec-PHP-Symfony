e8ed8923c0ab3495c8f104dfde822a88945bf175
Sadeu Michel <michel.sadeu@yahoo.fr>
2021-01-11 20:16:39 +0100

    Renforcer le typage des champs de formulaires

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationType extends AbstractType
@@ -13,8 +15,8 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('userName')
-            ->add('email')
+            ->add('userName', TextType::class)
+            ->add('email', EmailType::class)
             ->add('password', PasswordType::class)
             ->add('confirmPassword',PasswordType::class)
         ;
